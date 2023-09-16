f3c21fa37ec0b96cf8b240abce362778cad68545
Nicholas David <onedavid.nicholas@gmail.com>
2023-09-16 20:58:44 +0300

    initial commit

--- app/api/validation/route.ts
+++ app/api/validation/route.ts
@@ -1,20 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
 import { NextResponse } from 'next/server';
 
 // Define a named function for handling POST requests
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        try {
-            const data = await req.body;
-            console.log('VALIDATION_DATA', data);
+export async function POST(req: Request) {
+    try {
+        const data = await req.json();
+        console.log('VALIDATION_DATA', data);
 
-            return NextResponse.json('Successful', { status: 200 });
-        } catch (error) {
-            console.error(error);
-            return NextResponse.json('Internal Server Error', { status: 500 });
-        }
-    } else {
-        res.status(500).send('Method not allowed');
+        return NextResponse.json('Successful', { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json('Internal Server Error', { status: 500 });
     }
 }
