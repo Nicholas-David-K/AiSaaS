d8d69609850a943029508e0d68a4860436a47364
Nicholas David <onedavid.nicholas@gmail.com>
2023-09-19 14:05:23 +0300

    initial commit

--- app/api/stkstatus/route.ts
+++ app/api/stkstatus/route.ts
@@ -11,7 +11,65 @@ export async function POST(req: Request) {
         const data = await req.json();
         console.log('CALLBACK_URL_DATA', data.Body.stkCallback.CallbackMetadata);
 
-        return NextResponse.json(data, { status: 200 });
+        if (!user || !userId) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
+
+        const callbackData = data.Body.stkCallback.CallbackMetadata.Item;
+
+        // Extract relevant data from the callback
+        const amount = callbackData.find(
+            (item: any) => item.Name === 'Amount'
+        )?.Value;
+        const mpesaReceiptNumber = callbackData.find(
+            (item: any) => item.Name === 'MpesaReceiptNumber'
+        )?.Value;
+        const transactionDate = callbackData.find(
+            (item: any) => item.Name === 'TransactionDate'
+        )?.Value;
+        const phoneNumber = callbackData.find(
+            (item: any) => item.Name === 'PhoneNumber'
+        )?.Value;
+
+        if (!amount || !mpesaReceiptNumber || !transactionDate || !phoneNumber) {
+            return NextResponse.json('Missing required data', { status: 400 });
+        }
+
+        let mpesaSubscription = await prismadb.mpesaSubscription.findUnique({
+            where: { userId: userId },
+        });
+
+        if (!mpesaSubscription) {
+            await prismadb.mpesaSubscription.create({
+                data: {
+                    userId: userId,
+                    subscriptionAmount: amount,
+                    mpesaReceiptNumber: mpesaReceiptNumber,
+                    phoneNumber: phoneNumber,
+                    mpesaCurrentPeriodEnd: new Date(
+                        new Date().setDate(new Date().getDate() + 30)
+                    ),
+                },
+            });
+        }
+
+        // Update the MpesaSubscription record
+        await prismadb.mpesaSubscription.update({
+            where: { userId },
+            data: {
+                subscriptionAmount: amount,
+                mpesaReceiptNumber,
+                mpesaCurrentPeriodEnd: new Date(
+                    (
+                        mpesaSubscription?.mpesaCurrentPeriodEnd || new Date()
+                    ).getTime() +
+                        30 * 24 * 60 * 60 * 1000
+                ),
+                phoneNumber: phoneNumber,
+            },
+        });
+
+        return NextResponse.json('Data saved successfully', { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json('Internal Server Error', { status: 500 });
