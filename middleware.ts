48976f1899659a4d3b2bb8fc962eeae0105d3828
Nicholas David <onedavid.nicholas@gmail.com>
2023-09-25 15:47:45 +0300

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,9 @@ import { authMiddleware } from '@clerk/nextjs';
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
     publicRoutes: ['/', '/api/webhook', '/api/confirmation', '/api/validation'],
+    ignoredRoutes: ['/((?!api|trpc))(_next|.+..+)(.*)', '/api/stkstatus'],
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/api/stkstatus'],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
