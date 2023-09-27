4cbe6bd79bea29440c851b8b9c3e4090b7e9b0f1
Nicholas David <onedavid.nicholas@gmail.com>
2023-09-27 11:25:03 +0300

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,5 @@ export default authMiddleware({
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/lib(.*)'],
 };
