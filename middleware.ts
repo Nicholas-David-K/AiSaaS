baa36f945f98527ade4d51d364b23e3ed3ac7cf5
Nicholas David <onedavid.nicholas@gmail.com>
2023-09-27 10:39:41 +0300

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,13 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook', '/api/confirmation', '/api/validation'],
+    publicRoutes: [
+        '/',
+        '/api/webhook',
+        '/api/confirmation',
+        '/api/validation',
+        '/api/stkstatus',
+    ],
 });
 
 export const config = {
